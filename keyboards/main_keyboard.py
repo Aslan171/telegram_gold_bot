a11a793fa3d9e711ffe33e8f9a6062b96188f6e4
Aslan171 <aslan.sharipov2010@gmail.com>
2026-01-03 23:16:43 +0500

    Update main_keyboard.py

--- keyboards/main_keyboard.py
+++ keyboards/main_keyboard.py
@@ -1,4 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def build_main_kb():
-    ReplyKeyboardMarkup(resize_keyboard=True)
+    kb = ReplyKeyboardMarkup(
+        keyboard=[  # обязательно
+            [KeyboardButton(text="💰 Баланс"), KeyboardButton(text="💸 Пополнить")],
+            [KeyboardButton(text="📤 Вывод"), KeyboardButton(text="ℹ️ Помощь")]
+        ],
+        resize_keyboard=True
+    )
+    return kb
