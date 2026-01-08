8ed16d693feb3c235ab552f3ba6f2438132f96f4
Aslan171 <aslan.sharipov2010@gmail.com>
2026-01-08 21:12:16 +0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
