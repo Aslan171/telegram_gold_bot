3bc55dcec14749c6f7c566f1e7565b0211ee4ae8
Aslan171 <aslan.sharipov2010@gmail.com>
2026-01-15 19:45:35 +0500

    Update withdraw_keyboard.py

--- keyboards/withdraw_keyboard.py
+++ keyboards/withdraw_keyboard.py
@@ -8,10 +8,10 @@ def withdraw_input_kb() -> ReplyKeyboardMarkup:
 # --- Меню после вывода ---
 def withdraw_after_kb() -> ReplyKeyboardMarkup:
     keyboard = [
-        [KeyboardButton("🌟Вывести"), KeyboardButton("💰Пополнить")],
-        [KeyboardButton("🔢Посчитать"), KeyboardButton("✅О боте")],
-        [KeyboardButton("📖Помощь и ответы"), KeyboardButton("✨Продать голду")],
-        [KeyboardButton("🕹️Сменить игру"), KeyboardButton("🆔Профиль")],
-        [KeyboardButton("📖Правила вывода Gold"), KeyboardButton("⬅ Назад")]
+        [KeyboardButton(text="🌟Вывести"), KeyboardButton(text="💰Пополнить")],
+        [KeyboardButton(text="🔢Посчитать"), KeyboardButton(text="✅О боте")],
+        [KeyboardButton(text="📖Помощь и ответы"), KeyboardButton(text="✨Продать голду")],
+        [KeyboardButton(text="🕹️Сменить игру"), KeyboardButton(text="🆔Профиль")],
+        [KeyboardButton(text="📖Правила вывода Gold"), KeyboardButton(text="⬅ Назад")]
     ]
     return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
